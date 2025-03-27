312c336bf1f4bfeee1b82dd45e73ce897a2ce6d8
Даниил Овсяников <dannebey9@gmail.com>
2025-03-28 01:41:22 +0300

    chore(code-gen): the code generator finalized

--- plopfile.js
+++ plopfile.js
@@ -8,20 +8,93 @@ export default function (plop) {
         name: "name",
         message: "Введите имя компонента (например, Button):",
       },
-    ],
-    actions: [
       {
-        type: "addMany",
-        destination: "src/components/{{pascalCase name}}",
-        base: "generator/templates/component",
-        templateFiles: "generator/templates/component/**/*.hbs",
+        type: "confirm",
+        name: "withStories",
+        message: "Создать Storybook-истории для компонента?",
+        default: true,
       },
       {
+        type: "confirm",
+        name: "withTests",
+        message: "Создать тесты (Jest + Testing Library)?",
+        default: true,
+      },
+      {
+        type: "confirm",
+        name: "withVars",
+        message: "Создать SCSS-файл с переменными для компонента?",
+        default: true,
+      },
+    ],
+    actions: [
+      // 1) Проверка наличия директории /src/components/{{pascalCase name}}
+      //    Если уже существует, генерация будет прервана.
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/index.ts",
+        templateFile: "generator/templates/component/index.ts.hbs",
+        abortOnFail: true,
+      },
+      // 2) Основной файл компонента
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+        templateFile: "generator/templates/component/component.tsx.hbs",
+        abortOnFail: true,
+      },
+      // 3) Файл стилей (module.scss)
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.module.scss",
+        templateFile: "generator/templates/component/component.module.scss.hbs",
+        abortOnFail: true,
+      },
+      // 4) Файл с локальными SCSS-переменными (создаётся, только если пользователь согласился)
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/_{{kebabCase name}}.vars.scss",
+        templateFile: "generator/templates/component/_component.vars.scss.hbs",
+        skip: (data) =>
+          data.withVars
+            ? undefined
+            : "Пользователь выбрал не создавать SCSS-переменные.",
+        abortOnFail: true,
+      },
+      // 5) Storybook-истории (создаётся, только если пользователь согласился)
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
+        templateFile: "generator/templates/component/component.stories.tsx.hbs",
+        skip: (data) =>
+          data.withStories
+            ? undefined
+            : "Пользователь выбрал не создавать stories.",
+        abortOnFail: true,
+      },
+      // 6) Тесты (создаются, только если пользователь согласился)
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
+        templateFile: "generator/templates/component/component.test.tsx.hbs",
+        skip: (data) =>
+          data.withTests
+            ? undefined
+            : "Пользователь выбрал не создавать тесты.",
+        abortOnFail: true,
+      },
+      // 7) Модификация (при необходимости) файла темы — например, если нужно добавить
+      //    @forward для переменных в глобальную тему. Используем pattern/modify.
+      {
         type: "modify",
-        path: "src/styles/theme.scss",
-        pattern: /(\/\* COMPONENT VARS IMPORTS \*\/)/g,
+        path: "src/styles/theme/theme.scss",
+        pattern: /(\/\* COMPONENT IMPORTS \*\/)/g,
         template:
-          '$1\n@forward "../components/{{pascalCase name}}/{{camelCase name}}.vars";',
+          '$1\n@forward "components/{{pascalCase name}}/_{{kebabCase name}}.vars.scss";',
+        skip: (data) =>
+          data.withVars
+            ? undefined
+            : "Пользователь выбрал не создавать SCSS-переменные, не добавляем forward в тему.",
       },
     ],
   });
